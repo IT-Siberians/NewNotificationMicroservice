4a19e720a465f1fbd0e11e1c5166fbbc4508655d
Barmalej <barmalej@inbox.ru>
2024-10-09 18:27:43 +0700

    hot fixed mapping

--- NewNotificationMicroservice.Infrastructure.Queues.Implementations/RabbitMQ/Mapper/QueueProfile.cs
+++ NewNotificationMicroservice.Infrastructure.Queues.Implementations/RabbitMQ/Mapper/QueueProfile.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using NewNotificationMicroservice.Application.Models.BusQueue;
+using NewNotificationMicroservice.Application.Models.User;
+using NewNotificationMicroservice.Common.Infrastructure.Queues.Events;
 using NewNotificationMicroservice.Infrastructure.RabbitMQ.Models;
 
 namespace NewNotificationMicroservice.Infrastructure.Queues.Implementations.RabbitMQ.Mapper
@@ -11,6 +13,8 @@ namespace NewNotificationMicroservice.Infrastructure.Queues.Implementations.Rabb
             CreateMap<BusQueue, BusQueueModel>()
                     .ForMember(d => d.QueueName, o => o.MapFrom(s => s.QueueName.Value))
                     .ReverseMap();
+            CreateMap<CreateUserEvent, CreateUserModel>().ReverseMap();
+            CreateMap<UpdateUserEvent, UpdateUserModel>().ReverseMap();
         }
     }
 }
